6e0a7165dab8634761d794987ae5d9c6daa77be4
Frederik <frederikfabritius@gmail.com>
2024-09-10 11:58:03 +0200

    Runnable with idx.google.com

---
+++ .idx/dev.nix
@@ -0,0 +1,21 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  packages = [ pkgs.python3 pkgs.libGL pkgs.xorg.libX11 pkgs.gcc8 ];
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" "ms-toolsai.jupyter"];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "README.md" "src/index.html" "main.py" ];
+      }; # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+  };
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Math fidget spinner
+Made a 3d printable math fidget spinner, with assignments on the top of the disc and the answers on the backside.
+
+Idea by Thomas, math teacher of my oldest daughter.
+
+You can adapt it yourself in your browser on mybinder: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/hantusk/math_fidget_spinner/HEAD?labpath=math_fidget_spinner.ipynb)
